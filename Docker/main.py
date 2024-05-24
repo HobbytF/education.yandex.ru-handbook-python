663383eadc9c848bd37513a7b4f02ef95af20355
HobbytF <HobbytFizmat@yandex.ru>
2024-05-24 15:39:01 +0300

    Added 2.4.

--- Docker/main.py
+++ Docker/main.py
@@ -1,3 +1,39 @@
-while (s := input()) != 'Три!':
-    print('Режим ожидания...')
-print('Ёлочка, гори!')
+n = int(input())
+s = ''
+j = 1
+k = 0
+razm = 0
+
+for i in range(1, n + 1):
+    if k < j:
+        k += 1
+        if k != j:
+            s = s + str(i) + ' '
+        else:
+            s = s + str(i)
+            if len(s) > razm:
+                razm = len(s)
+            s = ''
+            k = 0
+            j += 1
+if s != '':
+    if len(s) > razm:
+        razm = len(s)
+    
+s = ''
+j = 1
+k = 0
+
+for i in range(1, n + 1):
+    if k < j:
+        k += 1
+        if k != j:
+            s = s + str(i) + ' '
+        else:
+            s = s + str(i)
+            print(s.center(razm))
+            s = ''
+            k = 0
+            j += 1
+if s != '':
+    print(s.center(razm))
